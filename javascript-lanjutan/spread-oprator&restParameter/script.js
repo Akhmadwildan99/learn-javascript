438889b7186da4928f77e7bf8c0f2c42db7bcd06
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-05-10 15:49:33 +0700

    belajar soread

---
+++ javascript-lanjutan/spread-oprator&restParameter/script.js
@@ -0,0 +1,30 @@
+// Spread Oprator
+// memecah iterables menjdi single
+
+
+// console.log(...mhs[1]);
+
+// const mhs = ['wildan', 'roni', 'ronal'];
+// const dosen =['euln', 'romnga', 'danilo'];
+// const orang = [...mhs,'fuli', ...dosen];
+// // const orang = mhs.concat(dosen);
+// console.log(orang);
+
+
+
+
+// untuk mengkopi data:
+// const mhs = ['wildan', 'roni', 'ronal'];
+// const mhs1 = [...mhs];
+// mhs1[0] = 'tunam';
+// console.log(mhs);
+
+const liMhs = document.querySelectorAll('li');
+// const mhs= [];
+// for(let i = 0; i < liMhs.length; i++) {
+//     mhs.push(liMhs[i].textContent);
+// }
+// console.log(mhs);
+
+const mhs = [...liMhs].map(m => m.textContent);
+console.log(mhs);

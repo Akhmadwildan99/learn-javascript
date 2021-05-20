b4a0c99222e747e8d802f6f84cf8c4bcc0e4449f
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-05-20 21:49:13 +0700

    belajar rest parameter

--- javascript-lanjutan/spread-oprator&restParameter/script.js
+++ javascript-lanjutan/spread-oprator&restParameter/script.js
@@ -31,6 +31,52 @@
 
 
 
-const nama = document.querySelector('.nama');
-const huruf = [...nama.textContent].map(h => `<span>${h}</span>`).join('');
-nama.innerHTML = huruf;
+// const nama = document.querySelector('.nama');
+// const huruf = [...nama.textContent].map(h => `<span>${h}</span>`).join('');
+// nama.innerHTML = huruf;
+ 
+
+// function myFunc(a, b, ...myArgs) {
+//     return `a = ${a}, b = ${b}, myArgs = ${myArgs}`
+// }
+
+// console.log(myFunc(1,2,3,4,5,6));
+
+
+// Cara menghasilkan value array:
+
+
+// function func(...myArgs) {
+//     return myArgs;
+// }
+
+// console.log(func(1,3,4,5,6));
+
+
+// function func() {
+//     return Array.from(arguments);
+// }
+
+// console.log(func(1,3,4,5,6));
+
+
+
+// function func() {
+//     return [...arguments];
+// }
+
+// console.log(func(1,3,4,5,6));
+
+
+
+function jumlahkanAngka(...angka) {
+    let total = 0;
+    for(n of angka) {
+        total += n;
+    }
+
+    return total;
+
+}
+
+console.log(jumlahkanAngka(1,2,3,4,5,6,7));

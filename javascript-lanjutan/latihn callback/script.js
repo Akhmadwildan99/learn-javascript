153d29984df84e9568c942ad45399e1a63b7bbd6
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-05-21 22:00:30 +0700

    belajar callback

--- javascript-lanjutan/latihn callback/script.js
+++ javascript-lanjutan/latihn callback/script.js
@@ -10,7 +10,7 @@ $.ajax({
                             <div class="card-body">
                             <h5 class="card-title">${m.Title}</h5>
                             <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                            <a href="#" class="btn btn-primary modal-detail-button"  data-toggle="modal" data-target="#movieDetailModal">show details</a>
+                            <a href="#" class="btn btn-primary modal-detail-button"  data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${m.imdbID}">show details</a>
                             </div>
                         </div>
 

c73acb9d1cd7325774b520a4bf7386bb160bb616
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-05-21 11:15:46 +0700

    belajar callback async

--- javascript-lanjutan/async/script.js
+++ javascript-lanjutan/async/script.js
@@ -5,9 +5,40 @@
 //     alert(`Halo ${nama}`);
 // }
 
-function tampilkanPesan(callback) {
-    const nama = prompt('masukan nama: ');
-    callback(nama);
+// function tampilkanPesan(callback) {
+//     const nama = prompt('masukan nama: ');
+//     callback(nama);
+// }
+
+// tampilkanPesan(nama => alert(`Halo ${nama}`));
+
+
+
+// Asynchronus Callback
+
+function getDataKariyawan(url, success, error) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4) {
+            if(xhr.status === 200) {
+                success(xhr.response);
+            } else if(xhr.status === 404) {
+                error();
+            }
+        }
+    }
+
+
+    xhr.open('get', url);
+    xhr.send();
 }
 
-tampilkanPesan(nama => alert(`Halo ${nama}`));
+
+getDataKariyawan('data/kariyawan.jeson', results => {
+    console.log(results);
+
+}, () => {
+
+});
+
